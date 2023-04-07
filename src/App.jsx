f38c735eb6aa7f877ce47045bf03a83ca18236ac
chrcaceres <chrn.caceres@gmail.com>
2023-04-07 00:15:07 -0400

    Fixed App.jsx pagina Inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
-import Inicio from "./pages/inicio";
+import Inicio from "./pages/Inicio";
 import Contacto from "./pages/Contacto";
 import Blog from "./pages/Blog";
 import LayoutPublic from "./layouts/LayoutPublic";
